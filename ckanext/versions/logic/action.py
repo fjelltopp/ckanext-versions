815a79d5d10ea03c5b24f063173b5e2b49a690f8
Toavina <toavinahajaina@gmail.com>
2026-04-27 17:29:22 +0300

    Drop CKAN <2.10 Activity import fallbacks
    CKAN <2.10 is no longer supported, so the try/except fallback chain
    for importing Activity from ckan.model is dead code.

--- ckanext/versions/logic/action.py
+++ ckanext/versions/logic/action.py
@@ -12,15 +12,7 @@ from sqlalchemy.exc import IntegrityError
 
 from ckanext.versions.model import Version
 
-# Activity import for different CKAN versions
-# CKAN 2.10+ moved Activity to a separate plugin
-try:
-    from ckanext.activity.model import Activity
-except ImportError:
-    try:
-        from ckan.model import Activity
-    except ImportError:
-        Activity = core_model.Activity
+from ckanext.activity.model import Activity
 
 log = logging.getLogger(__name__)
 
--- ckanext/versions/logic/dataset_version_action.py
+++ ckanext/versions/logic/dataset_version_action.py
@@ -9,15 +9,7 @@ from ckan.plugins import toolkit
 from ckanext.versions.logic.action import version_show
 from ckanext.versions.model import Version
 
-# Activity import for different CKAN versions
-# CKAN 2.10+ moved Activity to a separate plugin
-try:
-    from ckanext.activity.model import Activity
-except ImportError:
-    try:
-        from ckan.model import Activity
-    except ImportError:
-        Activity = core_model.Activity
+from ckanext.activity.model import Activity
 
 log = logging.getLogger(__name__)
 
